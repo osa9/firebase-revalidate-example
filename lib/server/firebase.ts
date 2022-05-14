2a014461d5913e04adcc74449efed02c10f91fb6
osa9 <osanine9@gmail.com>
2022-05-14 21:47:38 +0900

    initial commit

--- lib/server/firebase.ts
+++ lib/server/firebase.ts
@@ -2,6 +2,7 @@ export const firebase_revalidate = async (path: string) => {
   const baseUri = process.env.BASE_URI
   if (!baseUri) throw new Error('BASE_URI is not set')
 
+  console.log(`PURGE ${baseUri}${path}`)
   const res = await fetch(`${baseUri}${path}`, {method: 'PURGE'})
   if (res.status !== 200) throw new Error(await res.text())
 }
